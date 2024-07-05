13c59aeaee845a7ba705c1d01cf884e41d1bf9c0
azmi adan <azmi.adan2032@gmail.com>
2024-07-05 13:00:03 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// index.js
+// Remove the <main> element with an id of "main"
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element and assign it to the newHeader variable
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Azmi Adan is the champion'; // Replace John Doe with your actual name
+
+// Add the newHeader element to the DOM
+document.body.appendChild(newHeader);
